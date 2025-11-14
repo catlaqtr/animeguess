71af19a868f9b2979856dcf5c998a4776c99fc4a
Mete Unal <meteunal46@hotmail.com>
2025-11-14 09:16:35 -0500

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -16,7 +16,7 @@ const contentSecurityPolicy = `
   frame-ancestors 'none';
   object-src 'none';
   base-uri 'self';
-  form-action 'self' https://formsubmit.co;
+  form-action 'self';
   worker-src 'self' blob:;
 `;
 
